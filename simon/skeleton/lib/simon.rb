2f41eb6639944ba6e898e816629790ee60cd15bd
JohnnyP <wabanichi@gmail.com>
2019-01-09 13:29:15 -0600

    finished simon game

--- simon/skeleton/lib/simon.rb
+++ simon/skeleton/lib/simon.rb
@@ -16,7 +16,7 @@ class Simon
     end
 
     game_over_message
-    @reset_game
+    reset_game
   end
 
   def take_turn
@@ -25,16 +25,30 @@ class Simon
 
     unless @game_over
       round_success_message
-      sequence_length += 1
+      @sequence_length += 1
     end
   end
 
   def show_sequence
     add_random_color
+    @seq.each do |color|
+      puts color
+      sleep 0.75
+      system("clear")
+      sleep 0.25
+    end
   end
 
   def require_sequence
-
+    puts "Repeat the sequence by entering the first letter of each color on a new line"
+    @seq.each do |color|
+      user_color = gets.chomp
+      if color[0] != user_color
+        @game_over = true
+        break
+      end
+    end
+    sleep 0.25
   end
 
   def add_random_color
