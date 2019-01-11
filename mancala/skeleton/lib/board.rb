8c3326afaa89ff645ece2ed4fedc6a3d152cb504
JohnnyP <wabanichi@gmail.com>
2019-01-11 11:34:19 -0600

    finished Board class

--- mancala/skeleton/lib/board.rb
+++ mancala/skeleton/lib/board.rb
@@ -23,10 +23,33 @@ class Board
   end
 
   def make_move(start_pos, current_player_name)
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
+    cup_idx = start_pos
+
+    until stones.empty?
+      cup_idx += 1
+      cup_idx = 0 if cup_idx > 13
+      if cup_idx == 6
+        @cups[6] << stones.pop if current_player_name == @name1
+      elsif cup_idx == 13
+        @cups[13] << stones.pop if current_player_name == @name2
+      else
+        @cups[cup_idx] << stones.pop
+      end
+    end
+    render
+    next_turn(cup_idx)
   end
 
   def next_turn(ending_cup_idx)
-    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elsif @cups[ending_cup_idx].count == 1
+      :switch
+    else
+      ending_cup_idx
+    end  
   end
 
   def render
@@ -38,8 +61,17 @@ class Board
   end
 
   def one_side_empty?
+    @cups[0..5].all? { |cup| cup.empty? } ||
+    @cups[7..12].all? { |cup| cup.empty? }
   end
 
   def winner
+    if @cups[13].count == @cups[6].count 
+      :draw
+    elsif @cups[6].count > @cups[13].count 
+      @name1
+    else
+      @name2
+    end
   end
 end
